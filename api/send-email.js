953a295aa0549f06bed6f6dc6f6ca196c1e4b250
Carol Levtchenko <carollevtchenko@192.168.0.43>
2025-07-29 17:23:13 -0300

    fix: quebra de parágrafo

--- api/send-email.js
+++ api/send-email.js
@@ -5,7 +5,15 @@ import { Resend } from 'resend'
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*") // CORS liberado para qualquer origem
+  // CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+
+  // Responde prévia de CORS (preflight)
+  if (req.method === "OPTIONS") {
+    return res.status(200).end()
+  }
 
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Método não permitido' })
