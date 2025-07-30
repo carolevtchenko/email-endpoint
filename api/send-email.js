9ddfe35c00b176890ec39f115c64927b8c45d3ac
Carol Levtchenko <carollevtchenko@192.168.0.43>
2025-07-30 10:49:22 -0300

    fix: text and link can be changed in the framer UI

--- api/send-email.js
+++ api/send-email.js
@@ -1,16 +1,12 @@
-// api/send-email.js
-
 import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export default async function handler(req, res) {
-  // Libera CORS para qualquer origem (como o Framer)
   res.setHeader("Access-Control-Allow-Origin", "*")
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
   res.setHeader("Access-Control-Allow-Headers", "Content-Type")
 
-  // Responde a requisições OPTIONS (preflight CORS)
   if (req.method === "OPTIONS") {
     return res.status(200).end()
   }
@@ -19,25 +15,24 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Método não permitido' })
   }
 
-  const { to_email, message, link } = req.body
+  const { to_email, message, link, linkLabel } = req.body
 
-  if (!to_email || !message || !link) {
+  if (!to_email || !message || !link || !linkLabel) {
     return res.status(400).json({ error: 'Campos obrigatórios ausentes' })
   }
 
   try {
-    const formattedMessage = message.replace(/\/n/g, "<br />")
+    const formattedMessage = message.replace(/\n/g, "<br />")
 
-
-const data = await resend.emails.send({
-  from: 'Carol Levtchenko <reminder@carol-levtchenko.com>',
-  to: to_email,
-  subject: 'Portfolio - Senior Product Designer',
-  html: `
-    <p>${formattedMessage}</p>
-    📎 <a href="https://github.com/carolevtchenko/carol-levtchenko-cv/raw/main/CarolLevtchenko_ProductDesigner_Resume.pdf" target="_blank">Anexei também meu CV por aqui</a>.
+    const data = await resend.emails.send({
+      from: 'Carol Levtchenko <reminder@carol-levtchenko.com>',
+      to: to_email,
+      subject: 'Portfolio - Senior Product Designer',
+      html: `
+        <p>${formattedMessage}</p>
+        <p><a href="${link}" target="_blank">${linkLabel}</a></p>
       `
-})
+    })
 
     return res.status(200).json({ success: true, data })
   } catch (err) {
