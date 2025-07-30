ac9112951ec396e068c89573821c77a138cdfc10
Carol Levtchenko <carollevtchenko@192.168.0.43>
2025-07-30 12:17:02 -0300

    fix: CORS problem

--- api/send-email.js
+++ api/send-email.js
@@ -3,9 +3,11 @@ import { Resend } from 'resend'
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Origin", "https://carol-levtchenko.com")
+  res.setHeader("Access-Control-Allow-Credentials", "true")
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
   res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+
 
   if (req.method === "OPTIONS") {
     return res.status(200).end()
